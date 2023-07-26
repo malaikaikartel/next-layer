113108ffd36af3ea89f7d2b985065b1755956f46
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-07-26 21:31:10 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   strategy="afterInteractive"
 />
 
-<AdsComponent dataAdSlot='X7XXXXXX5X' />
+
       </body>
     </html>
   )
