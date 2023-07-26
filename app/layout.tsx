951e4127dd399535d53fe4fc0e4c01e451076e2f
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-07-26 18:46:19 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@/components/analytics"
 import { ModeToggle } from "@/components/mode-toggle"
-import Script from "next/script"
+
 
 const inter = Inter({ subsets: ["latin"] })
 
