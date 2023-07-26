2b9a020c3527d09ee62ad64ee414f261ab09b75d
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-07-26 19:07:24 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@/components/analytics"
 import { ModeToggle } from "@/components/mode-toggle"
-import AdsComponent from './AdsComponent';
+import AdsComponent from "./AdsComponent";
 
 const inter = Inter({ subsets: ["latin"] })
 
