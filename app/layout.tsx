5d569613f4cbcb6b0c76270decc19762a953b201
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-07-26 19:06:24 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@/components/analytics"
 import { ModeToggle } from "@/components/mode-toggle"
-
+import AdsComponent from './AdsComponent';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,6 +20,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <AdsComponent dataAdSlot="6389784149267865" />
       <body
         className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
       >
